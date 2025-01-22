693366f4dac52838df26643f11fa71803c6dc691
Leah123-d <95731243+Leah123-d@users.noreply.github.com>
2025-01-22 10:35:25 -0800

    Converted on list Item to checkbox

--- recipe.js
+++ recipe.js
@@ -1,7 +1,5 @@
-addEventListener("load",(event) => {
-    //I have to make sure my function are at the top of the page to run? I need to review and think about order or functions.
-
 const myDiv = document.getElementById("myDiv");
+const myListItem1 = document.getElementById("listItem1")
 
 //creating checkbox element
 const checkbox = document.createElement("input"); //can I have this copy text of the ingredient and that reassign the text content? 
@@ -12,6 +10,9 @@ checkbox.type = "checkbox";
 checkbox.name = "name";
 checkbox.value = "value";
 checkbox.id = "id";
+
+
+
 
 //creating label for checkbox 
 const label = document.createElement("label");
@@ -25,20 +26,28 @@ label.htmlFor = "id"
 //the created label tag 
 
 label.appendChild(
-    document.createTextNode("this creates the label for checkbox.")
+   document.createTextNode(myListItem1.textContent),
+
 );
+//this is interesting, I want to replace the text list item
+
+const updateList1 = document.getElementById("listItem1").textContent = ""; //this is interesting, but is there a better way to do it?
+
+
 
 // appending the checkbox
 //and label to the div 
 
-myDiv.appendChild(checkbox);
-myDiv.appendChild(label);
+//myDiv.appendChild(checkbox);
+//myDiv.appendChild(label);
+
+myListItem1.appendChild(checkbox);
+myListItem1.appendChild(label);
 
 
-});
 
-//will add an on-click event listener to this function 
-function ingredients(){
+
+/*function ingredients(){
     const ingredientList = document.getElementsByTagName("ul");
     for(const ul of ingredientList){
         ul.classList.add("checked");
@@ -47,10 +56,14 @@ function ingredients(){
     return ingredientList;
 
 }
-    ingredients();
+    ingredients();*/
 
 //I want to adjust this so it is applying the checkbox to the ingredient list. 
 //Can I adjust the code to loop through each child element and apply the checkbox? 
+
+//what do we know so far?
+//would I need to modify the text content of my list item to have the box in front of the text? 
+//I will need to understand what each item is doing to modify it too. 
 
 
 
