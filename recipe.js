7086dcbfd2982362fc3f30af187f9f4a9bba422a
Leah123-d <95731243+Leah123-d@users.noreply.github.com>
2025-01-23 11:14:42 -0800

    Updated padding on nav bar

--- recipe.js
+++ recipe.js
@@ -1,4 +1,16 @@
-//console.log(navItemDropdown);
+
+/*1.implement 2 other DOM manipulation methods 
+
+2.back to basics, look up 2 resources how to style check boxes and crossing them off 
+3. Very last adds Jumbotron with bootstrap 
+
+To do List: 
+1/ work on figuring out how to convert each list item to checkbox
+maybe if we get one working then we can apply to the rest 
+2/use DOM manipulation to cross out words of text box 
+3/implement additional DOM manipulation: 2. convert ingredients to metric 3. update nav bar to move simliar recipes to nav bar drop down
+4/prepare for presentation 
+additional DOM ideas, jump to recipe, jump to video? */
 
 function myFunction(){
     document.getElementById("myDropdown").classList.toggle("show");
@@ -20,64 +32,6 @@ function myFunction(){
 
 /*
 
-1.implement 2 other DOM manipulation methods 
-
-2.back to basics, look up 2 resources how to style check boxes and crossing them off 
-3. Very last adds Jumbotron with bootstrap 
-
-To do List: 
-1/ work on figuring out how to convert each list item to checkbox
-maybe if we get one working then we can apply to the rest 
-2/use DOM manipulation to cross out words of text box 
-3/implement additional DOM manipulation: 2. convert ingredients to metric 3. update nav bar to move simliar recipes to nav bar drop down
-4/prepare for presentation 
- 
-
-const myDiv = document.getElementById("myDiv");
-const myListItem1 = document.getElementById("listItem1")
-
-//creating checkbox element
-const checkbox = document.createElement("input"); //can I have this copy text of the ingredient and that reassign the text content? 
-
-// assigning the attributes 
-//to created checkbox 
-checkbox.type = "checkbox";
-checkbox.name = "name";
-checkbox.value = "value";
-checkbox.id = "id";
-
-
-
-
-//creating label for checkbox 
-const label = document.createElement("label");
-
-//assigning the created text to 
-//the created label tag
-
-label.htmlFor = "id"
-
-//appending the created text to
-//the created label tag 
-
-label.appendChild(
-   document.createTextNode(myListItem1.textContent),
-
-);
-//this is interesting, I want to replace the text list item
-
-const updateList1 = document.getElementById("listItem1").textContent = ""; //this is interesting, but is there a better way to do it?
-
-
-
-// appending the checkbox
-//and label to the div 
-
-//myDiv.appendChild(checkbox);
-//myDiv.appendChild(label);
-
-myListItem1.appendChild(checkbox);
-myListItem1.appendChild(label);
 
 
 
