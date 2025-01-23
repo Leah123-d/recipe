d4437c1c5adf87453aeed34dfe1f7553d6d57417
Leah123-d <95731243+Leah123-d@users.noreply.github.com>
2025-01-22 22:17:03 -0800

    Add dropdown menu items to nav bar

--- recipe.js
+++ recipe.js
@@ -1,17 +1,29 @@
+//let's add an event listener on the class for nav bar to add links 
 
+const nav = document.getElementById("navbarDropdown");
+
+//console.log(navItemDropdown);
+
+function myFunction(){
+    nav.classList.toggle("show");
+}
 
 
 
 /*
+
+1.implement 2 other DOM manipulation methods 
+
+2.back to basics, look up 2 resources how to style check boxes and crossing them off 
+3. Very last adds Jumbotron with bootstrap 
+
 To do List: 
 1/ work on figuring out how to convert each list item to checkbox
 maybe if we get one working then we can apply to the rest 
 2/use DOM manipulation to cross out words of text box 
 3/implement additional DOM manipulation: 2. convert ingredients to metric 3. update nav bar to move simliar recipes to nav bar drop down
 4/prepare for presentation 
-
-
-
+ 
 
 const myDiv = document.getElementById("myDiv");
 const myListItem1 = document.getElementById("listItem1")
