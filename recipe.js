6cc91a3c4ebddfb975ce73f66aa2af8ee09cd8bf
Leah123-d <95731243+Leah123-d@users.noreply.github.com>
2025-01-24 12:22:26 -0800

    Adds .gitignore file to project folder

--- recipe.js
+++ recipe.js
@@ -1,46 +1,11 @@
-
-/*1.implement 2 other DOM manipulation methods 
-
-2.back to basics, look up 2 resources how to style check boxes and crossing them off 
-3. Very last adds Jumbotron with bootstrap 
-
-To do List: 
-1/ work on figuring out how to convert each list item to checkbox
-maybe if we get one working then we can apply to the rest 
-2/use DOM manipulation to cross out words of text box 
-3/implement additional DOM manipulation: 2. convert ingredients to metric 3. update nav bar to move simliar recipes to nav bar drop down
-4/prepare for presentation 
-additional DOM ideas, jump to recipe, jump to video? */
-
-//created text content selector query for Ul 
-//append new item 
-//query selected ul items list items (lis) 
-//for loop through the length created an element input 
-//label variable, for every label there is an id
-//insert a check in front of of the ul then check next sibling 
-
-/*function myFunction(){
+//to show and dropdown list
+function myFunction(){
     document.getElementById("myDropdown").classList.toggle("show");
 }
 
-const checkbox = document.getElementById("listitem1");
-const textToCross = document.getElementById("item");
 
-checkbox.addEventListener("change", function(){
-    if(this.checked){
-        textToCross.style.textDecoration = "line-through";
-    }
-    else{
-        textToCross.style.textDecoration = "none";
-    }
-});*/
-
-
-
-//console.log(checkbox);
+//to add strike through each list item
 const item = document.querySelectorAll('#ingredientlist1 input[type="checkbox"], #ingredientlist2 input[type="checkbox"]');
-
-//console.log(item);
 
 for(let i = 0; i < item.length; i++){
     //console.log(item[i]);
@@ -57,22 +22,10 @@ for(let i = 0; i < item.length; i++){
     });
 }
 
-//selected checkbox
-//next element next sibling 
 
 
-
-//if we were to apply this to each item I think we can loop through them. Let's try that. 
-//query selector for checkboxes, iterate through that 
-//how would I write this out in plain english? 
-
-//for any item with class = item, if it is check apply line through decoration
-//I need to select the items with the check box 
-//I need the if statement to check each class item to see if they are checked 
-
-//will add an event listener to the button to display metric list
-
-/*const metricBtn = document.getElementById("metricBtn");
+//to display metric list
+const metricBtn = document.getElementById("metricBtn");
 const divMetric = document.getElementById("divMetric");
 const metric2 = document.getElementById("metric2");
 metric2.style.display="none"
@@ -96,16 +49,6 @@ metricBtn.addEventListener("click", function(){
     }
 
 });
-
-/*const hide = document.getElementById("hide");
-
-hide.addEventListener("click", function(event){
-    document.getElementById("divMetric").style.display = "none";
-
-
-
-
-})*/
 
 
 
